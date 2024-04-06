c1dfc860a6e45750fa361e6a4b8422f69c204ce4
Angelo Mahrouchi <angelo.mahrouchi@ankorstore.com>
2024-04-06 19:01:03 +0200

    feat(http api): create the http server

--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"github.com/labstack/echo/v4"
 	"github.com/rs/zerolog/log"
 )
 
 func main() {
 	log.Info().Msg("Starting Serena node...")
 
-	// TODO: start the API server
-	log.Info().Msg("Starting API server...")
-
 	// TODO: start the block routine
 	log.Info().Msg("Starting block producer...")
+
+	// Start the API server
+	log.Info().Msg("Starting API server...")
+	e := echo.New()
+	e.GET("/healthz", func(c echo.Context) error {
+		return c.JSON(200, map[string]string{"status": "ok"})
+	})
+	e.Logger.Fatal(e.Start(":8080")) // TODO: get the port from a config file or env variable (or both)
 
 	log.Info().Msg("Serena node started!")
 }
