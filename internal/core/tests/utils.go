c93b66bcf01a7cd969049403aa4b7abbc6a16121
Angelo Mahrouchi <angelo.mahrouchi@ankorstore.com>
2024-04-15 23:08:46 +0200

    chore(chain): add comment on the test utils package

--- internal/core/tests/utils.go
+++ internal/core/tests/utils.go
@@ -2,6 +2,10 @@ package tests
 
 import "github.com/rs/zerolog"
 
+/*
+ * Utility package for tests.
+ */
+
 // NewEmptyLogger creates a new empty logger.
 func NewEmptyLogger() *zerolog.Logger {
 	logger := zerolog.New(nil).Level(zerolog.Disabled)
