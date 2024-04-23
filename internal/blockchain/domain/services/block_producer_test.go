a158dc9b36d85412d5f605b9845247bc1c9df9e2
Angelo Mahrouchi <angelo.mahrouchi@ankorstore.com>
2024-04-23 06:51:00 +0200

    test(producer): test the CalculateHash function

--- internal/blockchain/domain/services/block_producer_test.go
+++ internal/blockchain/domain/services/block_producer_test.go
@@ -1,3 +1,28 @@
 package services_test
 
-// TODO: test
+import (
+	"github.com/amahrouchi/serena/internal/blockchain/domain/services"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+// BlockProducerSuite is the test suite for the BlockProducer struct.
+type BlockProducerSuite struct {
+	suite.Suite
+}
+
+// TestCalculateHash tests the CalculateHash method.
+func (bps *BlockProducerSuite) TestCalculateHash() {
+	producer := services.NewBlockProducer(nil, nil)
+
+	// Calculate hash
+	hash := producer.CalculateHash(nil)
+
+	// Assert
+	bps.Equal("", hash)
+}
+
+// TestGetLastBlock runs the BlockProducerSuite.-
+func TestBlockProducerSuite(t *testing.T) {
+	suite.Run(t, new(BlockProducerSuite))
+}
