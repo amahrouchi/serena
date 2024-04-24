eb7538d7b2538524cbb0b998a571bcb74ddaf11d
Angelo Mahrouchi <angelo.mahrouchi@ankorstore.com>
2024-04-24 15:34:52 +0200

    test(time sync): test TimeSync struct

--- internal/core/tools/time_sync_test.go
+++ internal/core/tools/time_sync_test.go
@@ -1,3 +1,33 @@
 package tools_test
 
-// TODO: Implement the tests.
+import (
+	"github.com/amahrouchi/serena/internal/core/tools"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+// TimeSyncTestSuite is the test suite for the TimeSync struct.
+type TimeSyncTestSuite struct {
+	suite.Suite
+}
+
+// TestNewTimeSync tests the NewTimeSync method.
+func (s *TimeSyncTestSuite) TestNewTimeSync() {
+	timeSync := tools.NewTimeSync()
+	s.Assert().NotNil(timeSync)
+}
+
+// TestCurrent tests the Current method.
+func (s *TimeSyncTestSuite) TestCurrent() {
+	timeSync := tools.NewTimeSync()
+
+	currTime, err := timeSync.Current()
+
+	s.Assert().NoError(err)
+	s.Assert().NotNil(currTime)
+}
+
+// TestTimeSyncTestSuite tests the TimeSyncTestSuite.
+func TestTimeSyncTestSuite(t *testing.T) {
+	suite.Run(t, new(TimeSyncTestSuite))
+}
