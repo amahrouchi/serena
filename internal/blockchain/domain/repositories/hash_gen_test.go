d869724c992d53319f077fe02db4556ea16058fc
Angelo Mahrouchi <angelo.mahrouchi@ankorstore.com>
2024-05-03 07:48:24 +0200

    test(hash gen): tests the hash generator constructor

--- internal/blockchain/domain/repositories/hash_gen_test.go
+++ internal/blockchain/domain/repositories/hash_gen_test.go
@@ -1,3 +1,27 @@
 package repositories_test
 
-// TODO: implement this test
+import (
+	"github.com/amahrouchi/serena/internal/blockchain/domain/repositories"
+	"github.com/amahrouchi/serena/internal/core/tests"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+// HashGenSuite is the test suite for the HashGen struct.
+type HashGenSuite struct {
+	suite.Suite
+}
+
+// TestNewHashGen tests the NewHashGen method.
+func (hgs *HashGenSuite) TestNewHashGen() {
+	hashGen := repositories.NewHashGen(
+		tests.NewEmptyLogger(),
+	)
+
+	hgs.NotNil(hashGen)
+}
+
+// TestHashGenSuite tests the HashGenSuite.
+func TestHashGenSuite(t *testing.T) {
+	suite.Run(t, new(HashGenSuite))
+}
