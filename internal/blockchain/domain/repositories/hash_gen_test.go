706c15d841242772a37d746def9d88d5585a950a
Angelo Mahrouchi <angelo.mahrouchi@ankorstore.com>
2024-05-03 08:12:10 +0200

    test(hash gen): test the hash generation from a test block

--- internal/blockchain/domain/repositories/hash_gen_test.go
+++ internal/blockchain/domain/repositories/hash_gen_test.go
@@ -1,8 +1,11 @@
 package repositories_test
 
 import (
+	"fmt"
+	"github.com/amahrouchi/serena/internal/blockchain/domain/models"
 	"github.com/amahrouchi/serena/internal/blockchain/domain/repositories"
 	"github.com/amahrouchi/serena/internal/core/tests"
+	"github.com/samber/lo"
 	"github.com/stretchr/testify/suite"
 	"testing"
 )
@@ -19,6 +22,37 @@ func (hgs *HashGenSuite) TestNewHashGen() {
 	)
 
 	hgs.NotNil(hashGen)
+}
+
+// TestFromBlock tests the FromBlock method.
+func (hgs *HashGenSuite) TestFromBlock() {
+	hgs.Run("nil block", func() {
+		hashGen := repositories.NewHashGen(
+			tests.NewEmptyLogger(),
+		)
+
+		_, err := hashGen.FromBlock(nil)
+
+		hgs.Error(err)
+	})
+
+	hgs.Run("valid block", func() {
+		hashGen := repositories.NewHashGen(
+			tests.NewEmptyLogger(),
+		)
+
+		hash, err := hashGen.FromBlock(&models.Block{
+			ID:           1,
+			PreviousHash: lo.ToPtr("previous_hash"),
+			Payload:      "{\"data\":\"test\"}",
+			Status:       models.BlockStatusPending,
+		})
+
+		fmt.Printf("hash: %s\n", hash)
+
+		hgs.NoError(err)
+		hgs.Equal("5f0d96cd38ace8977d488c7b3b8dcef2414f08c6c869371f269255c9af50bd10", hash)
+	})
 }
 
 // TestHashGenSuite tests the HashGenSuite.
