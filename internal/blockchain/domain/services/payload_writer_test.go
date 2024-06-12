f470f0049cafd2108b81f3de3f4104eb2c7171bd
Angelo Mahrouchi <angelo.mahrouchi@ankorstore.com>
2024-06-12 22:44:29 +0200

    test(payload writer): test the Write func

--- internal/blockchain/domain/services/payload_writer_test.go
+++ internal/blockchain/domain/services/payload_writer_test.go
@@ -1,3 +1,64 @@
 package services_test
 
-// TODO: implement the tests
+import (
+	"errors"
+	"github.com/amahrouchi/serena/internal/blockchain/domain/repositories"
+	"github.com/amahrouchi/serena/internal/blockchain/domain/services"
+	"github.com/amahrouchi/serena/internal/core/tests"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+// PayloadWriterSuite is the test suite for the PayloadWriter struct.
+type PayloadWriterSuite struct {
+	suite.Suite
+}
+
+// TestWrite tests the Write method.
+func (s *PayloadWriterSuite) TestWrite() {
+	s.Run("should write data to the blockchain", func() {
+		// Mock the repo
+		repo := new(repositories.BlockRepositoryMock)
+		repo.On(
+			"AppendDataToActiveBlock",
+			"author",
+			map[string]interface{}{"data": "test"},
+		).Return(nil)
+
+		// Write data
+		writer := services.NewPayloadWriter(
+			tests.NewEmptyLogger(),
+			repo,
+		)
+		err := writer.Write("author", map[string]interface{}{"data": "test"})
+
+		// Assert
+		s.NoError(err)
+	})
+
+	s.Run("should return an error if the repo fails", func() {
+		// Mock the repo
+		repo := new(repositories.BlockRepositoryMock)
+		repo.On(
+			"AppendDataToActiveBlock",
+			"author",
+			map[string]interface{}{"data": "test"},
+		).Return(errors.New("repo error"))
+
+		// Write data
+		writer := services.NewPayloadWriter(
+			tests.NewEmptyLogger(),
+			repo,
+		)
+		err := writer.Write("author", map[string]interface{}{"data": "test"})
+
+		// Assert
+		s.Error(err)
+		s.Equal("repo error", err.Error())
+	})
+}
+
+// TestPayloadWriterSuite runs the PayloadWriterSuite.
+func TestPayloadWriterSuite(t *testing.T) {
+	suite.Run(t, new(PayloadWriterSuite))
+}
